25e35c8a3f00d31f4a3f1277877e71915072b75c
savandy <andicuu@gmail.com>
2017-03-09 12:41:16 +0100

    Creada clase normal java ARBOL, con sus atributos y metodos

---
+++ ARBOLES_VARIAS_VENTANAS/src/ARBOL.java
@@ -0,0 +1,14 @@
+
+public class ARBOL {
+
+	String nombre;
+	String zona;
+	Double altura;
+	public ARBOL(String nombre, String zona, Double altura) {
+		super();
+	}
+	public ARBOL(){
+		super();
+	}
+	
+}
